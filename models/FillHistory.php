0a1f6fd27d8eddd23bd10aa41efd17bd4fa57770
dneb <ya.dneb@yandex.ru>
2020-10-01 21:10:04 +0300

    База данных

---
+++ models/FillHistory.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "fill_history".
+ *
+ * @property int $id
+ * @property int $tank
+ * @property string $employee
+ * @property int $liters
+ * @property string $date
+ */
+class FillHistory extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'fill_history';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['tank', 'employee', 'liters'], 'required'],
+            [['tank', 'liters'], 'integer'],
+            [['date'], 'safe'],
+            [['employee'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'tank' => 'Tank',
+            'employee' => 'Employee',
+            'liters' => 'Liters',
+            'date' => 'Date',
+        ];
+    }
+}
---
+++ models/Tank.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tank".
+ *
+ * @property int $id
+ * @property int $fullness
+ * @property int $quantity
+ */
+class Tank extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tank';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['fullness', 'quantity'], 'integer'],
+            [['quantity'], 'required'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'fullness' => 'Fullness',
+            'quantity' => 'Quantity',
+        ];
+    }
+}
